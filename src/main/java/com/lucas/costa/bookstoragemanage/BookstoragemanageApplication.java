a01b5778862ac2ae3eb1d8d554abbb5a79094d43
lucascostadeviff <lucascostadev@gmail.com>
2021-08-20 13:32:18 -0300

    Minha primeira aplicação SprinngBoot v1.0

---
+++ src/main/java/com/lucas/costa/bookstoragemanage/BookstoragemanageApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.costa.bookstoragemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoragemanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoragemanageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/costa/bookstoragemanage/BookstoragemanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.costa.bookstoragemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoragemanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
