da60ef66ec2292c74f91739d4fada8579c81b220
lucascostadeviff <lucascostadev@gmail.com>
2021-09-20 13:25:42 -0300

    Managed Classes AuthorDTO, BookDTO e configuração do Pom.xml

--- src/main/java/com/lucas/costa/bookstoragemanage/dto/AuthorDTO.java
+++ src/main/java/com/lucas/costa/bookstoragemanage/dto/AuthorDTO.java
@@ -1,9 +1,14 @@
 package com.lucas.costa.bookstoragemanage.dto;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 
 @Data
 @Builder
@@ -11,13 +16,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class AuthorDTO {
 
+
     private Long id;
 
 
-
+    @NotBlank//Anotação que faz verificação do campo. O mesmo não pode ser em Branco.
+    @Size (max = 200)   //Anotação que nos permite dfinir um tamanho de entrada para o atributo.
     private String name;
 
-
+    @NotNull//Informa que o valor deste atributo não pode ser nulo.
+    @Size (max = 100)   //Anotação que nos permite dfinir um tamanho de entrada para o atributo.
     private Integer age;
 
 }
--- src/main/java/com/lucas/costa/bookstoragemanage/dto/BookDTO.java
+++ src/main/java/com/lucas/costa/bookstoragemanage/dto/BookDTO.java
@@ -1,9 +1,15 @@
 package com.lucas.costa.bookstoragemanage.dto;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
@@ -13,17 +19,32 @@ public class BookDTO {
 
     private Long id;
 
-
+    @NotBlank //Informa que este campo não pode ser em branco.
+    @Size(max=200)// Informa e especifica o tamanho dos dados que podem ser inseridos nesse campo.
     private String name;
 
+    @NotNull //Informa que o valor deste atributor não pode ser Nulo.
     private Integer pages;
 
+    @NotNull
     private Integer chapters;
 
+    @NotBlank
+    @Size(max = 100)
+
+    @NotBlank
+    @Size(max = 200)
     private  String isbn;
 
+    @NotBlank
+    @Size (max = 200)
+    //Essa anotação @Pattern serve para tratar uma exceção em um dado especifico.
+    @Pattern(regexp ="(?:ISBN(?:-10)?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$)[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$",
+            message = "ISBN format must be a valid format")
     private String publisherName;
 
+    @Valid //Anotação própria do BeanValidation para que todas as anotações acima definam seus usos.
+    @NotNull //Autor Objeto de Relacionamento não pode ser nullo.
     private AuthorDTO authorDTO;
 
 }
